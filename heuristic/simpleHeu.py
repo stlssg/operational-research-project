da6e49cb0b4b5c21b8192b27227eb1ec4c8ac63f
stlssg <stlssg@gmail.com>
2021-07-01 01:20:35 +0200

    correct the errors!!!

--- heuristic/simpleHeu.py
+++ heuristic/simpleHeu.py
@@ -4,6 +4,7 @@ import math
 import logging
 import functools
 import numpy as np
+import copy
 
 class SimpleHeu():
     def __init__(self, epsilong, dict_data):
@@ -31,38 +32,44 @@ class SimpleHeu():
         self.t_ub = C_total / sum_pk_djk # upper bound with relaxation
         self.t_l = (self.t_lb + self.t_ub) / 2
         
+        
         while self.t_ub - self.t_lb > self.eps:
-            of, sol_x, condition = SimpleHeu.sub_algorithm(self)
+            of, sol_x, condition = SimpleHeu.sub_algorithm(self, self.data)
             if condition == 'yes': # there exists a feasible solution
                 self.t_lb = self.t_l
+                final_of = of
+                final_sol = sol_x
+                final_condition = condition
             elif condition == 'no': # there's no feasible solutions
                 self.t_ub = self.t_l
             self.t_l = (self.t_lb + self.t_ub) / 2
             self.ite += 1
         
+        print(final_condition)
         end = time.time()
         comp_time = end - start
         
-        return of, sol_x, comp_time
+        return final_of, final_sol, comp_time
         
-    def sub_algorithm(self):
+    def sub_algorithm(self, data_original):
+        data_temp = copy.deepcopy(data_original)
         D_jk = [] # the demand for product k of destination j within time duration t_l
         S_jk = [] # the supply for product k to destination j
-        for j in range(self.data['num_destinations']):
+        for j in range(data_temp['num_destinations']):
             temp_D = []
             temp_S = []
-            for k in range(self.data['num_products']):
-                temp_D.append(math.ceil(self.t_l * self.data['demand'][j][k]))
+            for k in range(data_temp['num_products']):
+                temp_D.append(math.ceil(self.t_l * data_temp['demand'][j][k]))
                 temp_S.append(0.0)
             D_jk.append(temp_D)
             S_jk.append(temp_S)
             
         X = []
-        for j in range(self.data['num_destinations']):
+        for j in range(data_temp['num_destinations']):
             temp_compartment = []
-            for i in range(self.data['num_compartments']):
+            for i in range(data_temp['num_compartments']):
                 temp_product = []
-                for k in range(self.data['num_products']):
+                for k in range(data_temp['num_products']):
                     temp_product.append(0)
                 temp_compartment.append(temp_product)
             X.append(temp_compartment)
@@ -70,53 +77,46 @@ class SimpleHeu():
         i = 0
         j = 0
         k = 0    
-        output = "yes"
         while True:
             if S_jk[j][k] >= D_jk[j][k]:
                 i = 0
                 k += 1
-                if k > self.data['num_products']-1:
+                if k > data_temp['num_products']-1:
                     k = 0
                     j += 1
-                    if j > self.data['num_destinations']-1:
+                    if j > data_temp['num_destinations']-1:
                         break
             else:
-                if i == self.data['num_compartments']:
-                    output = 'no'
-                    break
-                # a = self.data['size_package'][k]
-                # b = D_jk[j][k]
-                # c = S_jk[j][k]
-                # d = self.data['capacity_compartments'][i]
-                if self.data['size_package'][k] * (D_jk[j][k] - S_jk[j][k]) > self.data['capacity_compartments'][i]:
-                    X[j][i][k] = int(self.data['capacity_compartments'][i] / self.data['size_package'][k])
+                if data_temp['size_package'][k] * (D_jk[j][k] - S_jk[j][k]) > data_temp['capacity_compartments'][i]:
+                    X[j][i][k] = int(data_temp['capacity_compartments'][i] / data_temp['size_package'][k])
                     S_jk[j][k] += X[j][i][k]
-                    self.data['capacity_compartments'][i] -= self.data['size_package'][k] * X[j][i][k]
+                    data_temp['capacity_compartments'][i] -= data_temp['size_package'][k] * X[j][i][k]
                 else:
                     X[j][i][k] = D_jk[j][k] - S_jk[j][k]
                     S_jk[j][k] += X[j][i][k]
-                    self.data['capacity_compartments'][i] -= self.data['size_package'][k] * X[j][i][k]
+                    data_temp['capacity_compartments'][i] -= data_temp['size_package'][k] * X[j][i][k]
                 i += 1
-                if i > self.data['num_compartments']-1:
+                if i > data_temp['num_compartments']-1:
+                    i = 0
                     k += 1
-                    if k > self.data['num_products']-1:
+                    if k > data_temp['num_products']-1:
                         k = 0
                         j += 1
-                        if j > self.data['num_destinations']-1:
+                        if j > data_temp['num_destinations']-1:
                             break
         
-        if output == 'yes':
-            for j in  range(self.data['num_destinations']):
-                for k in range(self.data['num_products']):
-                    if D_jk[j][k] > S_jk[j][k]:
-                        output = "no"
-                        break
+        output = "yes"
+        for j in  range(data_temp['num_destinations']):
+            for k in range(data_temp['num_products']):
+                if D_jk[j][k] > S_jk[j][k]:
+                    output = "no"
+                    break
         if output == "yes":
-            t_s = S_jk[0][0] / self.data['demand'][0][0]
-            for j in  range(self.data['num_destinations']):
-                for k in range(self.data['num_products']):
-                    if self.data['demand'] == 0: continue
-                    temp = S_jk[j][k] / self.data['demand'][j][k]
+            t_s = S_jk[0][0] / data_temp['demand'][0][0]
+            for j in  range(data_temp['num_destinations']):
+                for k in range(data_temp['num_products']):
+                    if data_temp['demand'] == 0: continue
+                    temp = S_jk[j][k] / data_temp['demand'][j][k]
                     if temp < t_s:
                         t_s = temp
             if t_s > self.t_l:

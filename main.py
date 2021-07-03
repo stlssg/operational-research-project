e7b0109710d2a2ed11f276fc181f4f15df274d82
stlssg <stlssg@gmail.com>
2021-07-03 16:48:55 +0200

    minor change

--- main.py
+++ main.py
@@ -30,39 +30,39 @@ if __name__ == '__main__':
     d3 = copy.deepcopy(dict_data)
     print(dict_data)
     
-    # # exact solution from gurobi
-    # prb = SimpleTruckLoading()
-    # of_exact, sol_exact, comp_time_exact = prb.solve(
-    #     dict_data,
-    #     time_limit = 5,
-    #     gap = 0.1 / 100,
-    #     verbose=True
-    # )
-    # print(of_exact, sol_exact, comp_time_exact)
+    # exact solution from gurobi
+    prb = SimpleTruckLoading()
+    of_exact, sol_exact, comp_time_exact = prb.solve(
+        dict_data,
+        time_limit = 5,
+        gap = 0.1 / 100,
+        verbose=True
+    )
+    print(of_exact, sol_exact, comp_time_exact)
     
-    # # first heuristic method and solution
-    # heu_1 = SimulationHeu(0.03, d1)
-    # of_heu1, sol_heu1, comp_time_heu1 = heu_1.solve(True)
-    # print(of_heu1, sol_heu1, comp_time_heu1)
+    # first heuristic method and solution
+    heu_1 = SimulationHeu(0.03, d1)
+    of_heu1, sol_heu1, comp_time_heu1 = heu_1.solve(True)
+    print(of_heu1, sol_heu1, comp_time_heu1)
     
-    # # second heuristic method and solution
-    # heu_2 = AddingOneByOneHeu(d2)
-    # of_heu2, sol_heu2, comp_time_heu2 = heu_2.solve()
-    # print(of_heu2, sol_heu2, comp_time_heu2)
+    # second heuristic method and solution
+    heu_2 = AddingOneByOneHeu(d2)
+    of_heu2, sol_heu2, comp_time_heu2 = heu_2.solve()
+    print(of_heu2, sol_heu2, comp_time_heu2)
     
-    # # third heuristic method and solution
-    # heu_3 = DP_Heu(d3)
-    # of_heu3, comp_time_heu3 = heu_3.solve()
-    # print(of_heu3, comp_time_heu3)
+    # third heuristic method and solution
+    heu_3 = DP_Heu(d3)
+    of_heu3, comp_time_heu3 = heu_3.solve()
+    print(of_heu3, comp_time_heu3)
 
-    # # printing results of a file
-    # file_output = open("./results/basic_result_4_all_methods.csv", "w")
-    # file_output.write("method, objective function result, computational time, solution\n")
-    # file_output.write("{}, {}, {}, {}\n".format("exact_solution_from_gurobi", of_exact, comp_time_exact, sol_exact))
-    # file_output.write("{}, {}, {}, {}\n".format("simulation and greedy heuristic", of_heu1, comp_time_heu1, sol_heu1))
-    # file_output.write("{}, {}, {}, {}\n".format("simulation by 'adding' heuristic", of_heu2, comp_time_heu2, sol_heu2))
-    # file_output.write("{}, {}, {}, {}\n".format("partial dynamic programming heuristic", of_heu3, comp_time_heu3, '-'))
-    # file_output.close()
+    # printing results of a file
+    file_output = open("./results/basic_result_4_all_methods.csv", "w")
+    file_output.write("method, objective function result, computational time, solution\n")
+    file_output.write("{}, {}, {}, {}\n".format("exact_solution_from_gurobi", of_exact, comp_time_exact, sol_exact))
+    file_output.write("{}, {}, {}, {}\n".format("simulation and greedy heuristic", of_heu1, comp_time_heu1, sol_heu1))
+    file_output.write("{}, {}, {}, {}\n".format("simulation by 'adding' heuristic", of_heu2, comp_time_heu2, sol_heu2))
+    file_output.write("{}, {}, {}, {}\n".format("partial dynamic programming heuristic", of_heu3, comp_time_heu3, '-'))
+    file_output.close()
     
     # # comparison for different seeds
     # result_exact = []
